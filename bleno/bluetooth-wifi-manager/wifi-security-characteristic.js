ddb84d74c176fcec98c13573e1cfbcaf35072ede
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2018-06-11 16:49:55 +0000

    Removing print lines and hard coded values.

--- bleno/bluetooth-wifi-manager/wifi-security-characteristic.js
+++ bleno/bluetooth-wifi-manager/wifi-security-characteristic.js
@@ -23,11 +23,9 @@ var WiFiSecurityCharacteristic = function() {
     value: null
   });
 
-  //this._value = new Buffer(0);
   var myEnc = JSON.stringify(constants.WiFiEncryptionValues.security);
-  //console.log("Encryption is: " + myEnc);
-  //this._value = new Buffer.from(myEnc);
-  this._value = '["NONE","WEP 128b ASCii","WEP 128b Pasephrase","LEAP","WEP","WPA2 Personal","WPA2 Enterprise"]';
+  this._value = new Buffer.from(myEnc);
+  //this._value = '["NONE","WEP 128b ASCii","WEP 128b Pasephrase","LEAP","WEP","WPA2 Personal","WPA2 Enterprise"]';
   console.log("Encryption is :  " + this._value);
   this._updateValueCallback = null;
 };
@@ -36,23 +34,20 @@ util.inherits(WiFiSecurityCharacteristic, BlenoCharacteristic);
 
 
 WiFiSecurityCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  //console.log('WiFi Encryption Characteristic - onReadRequest: HEX value = ' + this._value.toString('hex'));
-  //console.log('WiFi Encryption Characteristic - onReadRequest: value = ' + this._value);
+  var result = this.RESULT_SUCCESS;
+
+  console.log('WiFi Encryption Characteristic - onReadRequest: value = ' + this._value);
   console.log('offset is : ' + offset);
-  var data = new Buffer('0123456789 1123456789 2123456789 3123456789 4123456789 5123456789 6123456789 7123456789 81234567890 9123456789 0123456789', 'utf-8');
-  console.log('data length is: ' + data.length);
-
+  //var data = new Buffer('0123456789 1123456789 2123456789 3123456789 4123456789 5123456789 6123456789 7123456789 81234567890 9123456789 0123456789', 'utf-8');
+  //var data = new Buffer('["NONE","WEP 128b ASCii","WEP 128b Pasephrase","LEAP","WEP","WPA2 Personal","WPA2 Enterprise"]', 'utf-8');
+  var data = new Buffer(this._value, 'utf-8');
   if (offset > data.length) {
-    callback(this.RESULT_INVALID_OFFSET, null);
+    result = this.RESULT_INVALID_OFFSET;
   } else {
-
-  //var data = new Buffer('["NONE","WEP 128b ASCii","WEP 128b Pasephrase","LEAP","WEP","WPA2 Personal","WPA2 Enterprise"]', 'utf-8')
-  //var mystrsize = this._value.length;
-  //var data2 = new Buffer(mystrsize);
     data = data.slice(offset);
-
-    callback(this.RESULT_SUCCESS, data);
   }
+
+  callback(result, data);
 };
 
 
