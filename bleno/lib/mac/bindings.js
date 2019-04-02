98b93c58557f88a58c645d4a23595dcf8176b08b
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-04-02 14:37:01 +0100

    Removing redundant files from 'bleno', index file, test cases and adding our own package file for the libraries that we use.

--- bleno/lib/mac/bindings.js
+++
@@ -1,8 +0,0 @@
-var os = require('os');
-var osRelease = parseFloat(os.release());
-
-if (osRelease < 17 ) {
-  module.exports = require('./yosemite');
-} else {
-  module.exports = require('./highsierra');
-}
